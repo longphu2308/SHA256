bddd86cfe4aaf78f7c63a7e4b720b1469016b475
peeydiu <phulong2308@gmail.com>
2024-11-23 14:16:00 +0700

    Initial Commit

---
+++ SHA256/SHA256App.swift
@@ -0,0 +1,17 @@
+//
+//  SHA256App.swift
+//  SHA256
+//
+//  Created by Trần Long Phú on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SHA256App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SHA256Tests/SHA256Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SHA256Tests.swift
+//  SHA256Tests
+//
+//  Created by Trần Long Phú on 23/11/24.
+//
+
+import Testing
+@testable import SHA256
+
+struct SHA256Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
